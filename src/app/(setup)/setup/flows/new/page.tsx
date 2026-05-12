1b56ef91c55bbbc2233e59e2527abaa09710d51d
shyamli k <shyamliyadav1111@gmail.com>
2026-05-12 21:51:04 +0530

    Add files via upload

---
+++ src/app/(setup)/setup/flows/new/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FlowBuilderClient from '@/components/setup/FlowBuilderClient';
+
+export default function NewFlowPage() {
+  return (
+    <div className="h-full w-full">
+      <FlowBuilderClient />
+    </div>
+  );
+}
